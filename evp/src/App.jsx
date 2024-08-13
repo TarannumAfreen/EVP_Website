e6263fc1de72d9ff8e0e909e537b0603d836e52c
Tarannum Afreen <tarannnum11@gmail.com>
2024-08-13 17:44:07 +0530

    EVP Website

---
+++ evp/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Main from './components/Main'
+import Home from './components/Home'
+// import Footer from './components/Footer'
+
+const App = () => {
+  return (
+  
+    <div>
+      {/* <Main/> */}
+      <Home/>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ evp/src/components/Footer.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div class="bg-[#000000] relative m-[0_0_7487.5px_0] flex self-start p-[27px_91px_11px_91px] w-[1622px] box-sizing-border">
+    <div class="bg-[#000000] flex flex-col items-center p-[64px_29.2px_21px_80px] w-[1440px] h-[fit-content] box-sizing-border">
+      <div class="m-[0_50.8px_64px_0] flex flex-row justify-between w-[1280px] box-sizing-border">
+        <span class="m-[0_36px_0_0] w-[320px] break-words font-['Poppins'] font-semibold text-[24px] text-[#FFFFFF]">
+          Have any questions? <br />
+          Let’s connect!
+        </span>
+        <div class="m-[12px_0_12px_0] flex flex-row box-sizing-border">
+          <div class="rounded-[100px] border-[1px_solid_rgba(255,255,255,0.24)] m-[0_8px_0_0] flex flex-row p-[13px_0_13px_23px] w-[360px] box-sizing-border">
+            <div class="m-[0_10px_0_0] flex w-[20px] h-[20px] box-sizing-border">
+              <img class="w-[20px] h-[20px]" src="../assets/vectors/user_2_x2.svg" />
+            </div>
+            <span class="break-words font-['Poppins'] font-normal text-[14px] leading-[1.429] text-[rgba(255,255,255,0.72)]">
+              Enter your email
+            </span>
+          </div>
+          <div class="rounded-[100px] bg-[#169D53] flex p-[14px_0.8px_14px_0] w-[117px] box-sizing-border">
+            <span class="break-words font-['Poppins'] font-medium text-[14px] leading-[1.429] text-[#FFF9F9]">
+              Connect
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[rgba(255,255,255,0.32)] m-[0_50.8px_63px_0] w-[1280px] h-[0px]">
+      </div>
+      <div class="m-[0_0_2.5px_6px] flex flex-row justify-between w-[1324.8px] box-sizing-border">
+        <div class="m-[3.9px_0_0_0] flex flex-col box-sizing-border">
+          <div class="bg-[url('../assets/images/evp_new_logo_2.png')] m-[0_33px_12.5px_33px] self-start w-[251px] h-[126.1px]">
+          </div>
+          <span class="break-words font-['Poppins'] font-normal text-[25px] tracking-[-0.9px] leading-[2.7] text-[#FFFFFF]">
+            Incubating India - for the world!
+          </span>
+        </div>
+        <div class="m-[0_0_19.5px_0] flex flex-row box-sizing-border">
+          <div class="m-[0_64.4px_0_0] flex flex-col box-sizing-border">
+            <div class="m-[0_0_16px_0] inline-block self-start break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+              About Us 
+            </div>
+            <div class="flex flex-col w-[fit-content] box-sizing-border">
+              <div class="m-[0_0_18px_0] inline-block break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+                Our companies
+              </div>
+              <div class="m-[0_15.2px_18px_0] inline-block break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+                Job openings
+              </div>
+              <div class="m-[0_0_18px_0] inline-block self-start break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+                Internships
+              </div>
+              <span class="self-start break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+                News
+              </span>
+            </div>
+          </div>
+          <div class="m-[0_50.7px_42px_0] flex flex-col box-sizing-border">
+            <div class="m-[0_0_16px_0] inline-block self-start break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+              Events
+            </div>
+            <div class="flex flex-col w-[fit-content] box-sizing-border">
+              <div class="m-[0_1px_18px_0] inline-block break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+                Founders’ Friday
+              </div>
+              <div class="m-[0_0_18px_0] inline-block break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+                Builders’ Sunday
+              </div>
+              <span class="self-start break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+                IMF
+              </span>
+            </div>
+          </div>
+          <div class="m-[0_0_84px_0] flex flex-col box-sizing-border">
+            <div class="m-[0_0_16px_0] inline-block self-start break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+              Programs
+            </div>
+            <div class="flex flex-col w-[fit-content] box-sizing-border">
+              <div class="m-[0_0_18px_0] inline-block break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+                Pre-Incubation Program
+              </div>
+              <span class="self-start break-words font-['Poppins'] font-normal text-[16px] text-[#FFFFFF]">
+                Incubation Program
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[rgba(255,255,255,0.32)] m-[0_50.8px_39px_0] w-[1280px] h-[0px]">
+      </div>
+      <div class="m-[0_50.8px_0_0] flex flex-row justify-between w-[1280px] box-sizing-border">
+        <div class="m-[9.5px_10.5px_9.5px_0] inline-block w-[330px] break-words font-['Poppins'] font-normal text-[14px] text-[#FFFFFF]">
+          © Copyright Edventure Park. All Rights Reserved
+        </div>
+        <div class="flex flex-row w-[196px] box-sizing-border">
+          <div class="rounded-[100px] border-[1px_solid_rgba(4,5,5,0.24)] bg-[rgba(4,5,5,0.1)] m-[0_12px_0_0] flex p-[10.7px_10.7px_10.7px_10.7px] w-[40px] h-[40px] box-sizing-border">
+            <img class="w-[16.7px] h-[16.7px]" src="../assets/vectors/vector_377_x2.svg" />
+          </div>
+          <div class="rounded-[100px] border-[1px_solid_rgba(4,5,5,0.24)] bg-[rgba(4,5,5,0.1)] m-[0_12px_0_0] flex p-[9px_9px_9px_9px] w-[40px] h-[40px] box-sizing-border">
+            <img class="w-[20px] h-[20px]" src="../assets/vectors/facebook_1_x2.svg" />
+          </div>
+          <div class="rounded-[100px] border-[1px_solid_rgba(4,5,5,0.24)] bg-[rgba(4,5,5,0.1)] m-[0_12px_0_0] flex p-[12.1px_9.6px_10.9px_11.5px] w-[40px] h-[40px] box-sizing-border">
+            <img class="w-[16.9px] h-[15px]" src="../assets/vectors/vector_105_x2.svg" />
+          </div>
+          <div class="rounded-[100px] border-[1px_solid_rgba(4,5,5,0.24)] bg-[rgba(4,5,5,0.1)] flex p-[10.3px_10.3px_10.2px_10.3px] w-[40px] h-[40px] box-sizing-border">
+            <img class="w-[17.5px] h-[17.5px]" src="../assets/vectors/vector_30_x2.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Footer
